95c1a2ebf257e8a6bcea8d930948a7edbb364054
yerassylsagynov <yerassyl.sagynov@nu.edu.kz>
2020-08-06 06:31:38 +0600

    Updated profiles for JPA services, tested active profiles, closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.service.OwnerService;
 import guru.springframework.sfgpetclinic.service.PetService;
 import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetMapService.java
@@ -2,11 +2,13 @@ package guru.springframework.sfgpetclinic.service.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.service.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
